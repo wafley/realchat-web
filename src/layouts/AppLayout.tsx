6889d4136bf65bd6610dd19b969e9ccb88033d6d
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2026-07-09 15:09:59 +0700

    feat: add bg-background to page headers and search message in ChatRoom

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -6,7 +6,7 @@ export default function AppLayout() {
   return (
     <div className="flex h-screen flex-col lg:flex-row">
       <Sidebar />
-      <main className="flex flex-1 overflow-hidden">
+      <main className="flex flex-1 overflow-hidden pt-[env(safe-area-inset-top,0px)] lg:pt-0">
         <Outlet />
       </main>
       <MobileNav />
