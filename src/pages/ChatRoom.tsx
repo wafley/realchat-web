871ae7181c484431bccd926de555f1bdefe2f200
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2026-07-09 15:21:25 +0700

    fix: use wpp-deffault.png (user's file) instead of downloaded wpp-default.png

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -163,7 +163,7 @@ export default function ChatRoom() {
         </div>
       )}
 
-      <div className="flex-1 overflow-y-auto bg-[url('/wpp-default.png')] bg-repeat bg-black/60 bg-blend-multiply px-4 py-4">
+      <div className="flex-1 overflow-y-auto bg-[url('/wpp-deffault.png')] bg-repeat bg-black/60 bg-blend-multiply px-4 py-4">
         <div className="space-y-3">
           {filteredMessages.map((msg) => (
             <div
