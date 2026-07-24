f741970a9d1c08e69e76df0e060a8c204339833b
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2026-07-24 08:49:42 +0700

    refactor(friends): remove Requests page, move badge to notification bell

--- src/hooks/usePendingFriendRequestCount.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getPendingRequestCount } from '@/services/friends';
-
-export function usePendingFriendRequestCount() {
-  return useQuery({
-    queryKey: ['pendingFriendRequestCount'],
-    queryFn: getPendingRequestCount,
-  });
-}
